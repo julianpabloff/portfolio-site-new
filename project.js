0b4eff161a72bd71d0a6e49e59dd8f2f976543a3
Julian Pabloff <julianpabloff@gmail.com>
2021-05-21 04:29:12 -0700

    added a bunch of code then commented it out

--- project.js
+++ project.js
@@ -2,19 +2,46 @@ window.addEventListener('load', function() {
 	const projects = document.querySelector('div.project-container').children;
 	for (let project of projects) {
 		const children = project.children;
-		const banner = children.item(1);
+		const movingDiv = children.item(1);
 		const expand = children.item(2);
 		const seeMore = expand.children.item(0);
 		project.addEventListener('mouseenter', () => {
-			banner.style.bottom = '200px';
+			movingDiv.style.bottom = '200px';
 			seeMore.style.opacity = '1';
 		});
 		project.addEventListener('mouseleave', () => {
-			banner.style.bottom = '0px';
+			movingDiv.style.bottom = '0px';
 			seeMore.style.opacity = '0';
 		});
 		expand.addEventListener('mousedown', () => {
 			console.log('See more');
 		});
+
+		/*
+		const techIconsDiv = movingDiv.children.item(0).children.item(1);
+		// const icons = Array.from(techIconsDiv.children);
+		const icons = techIconsDiv.children;
+		// const labels = icons.pop();
+		const labels = techIconsDiv.lastElementChild;
+		console.log(icons);
+		console.log(labels);
+		for (let i = 0; i < icons.length - 1; i++) {
+			let img = icons[i];
+			let label = labels.children.item(i);
+			img.addEventListener('mouseenter', e => {
+				label.style.opacity = '1';
+			});
+			img.addEventListener('mousemove', e => {
+				// console.log(e.offsetX.toString() + ', ' + e.offsetY.toString());
+				label.style.left = (e.offsetX + (36 * i) + 20).toString() + 'px';
+				label.style.top = (e.offsetY - 50).toString() + 'px';
+			});
+			img.addEventListener('mouseleave', e => {
+				label.style.opacity = '0';
+				label.style.left = '0';
+				label.style.top = '0';
+			});
+		}
+		*/
 	}
 });
