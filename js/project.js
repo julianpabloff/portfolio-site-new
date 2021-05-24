67b756ced11da4f0d8d8c38a9ead2ba84212995d
Julian Pabloff <julianpabloff@gmail.com>
2021-05-24 07:58:29 -0700

    first draft of project info design

--- js/project.js
+++ js/project.js
@@ -1,16 +1,23 @@
 window.addEventListener('load', function() {
 	const html = document.querySelector('html');
 	const body = document.getElementById('main-container');
-	const modal = document.getElementById('project-modal');
 	const backdrop = document.getElementById('modal-background');
-	let modalStyle = modal.style;
+	// const modal = document.getElementById('modal');
+	let modal;
+	// let modalStyle = modal.style;
 	let backdropStyle = backdrop.style;
-	let modalTransition = 400;
-	let modalTransitionString = (modalTransition / 1000).toString() + 's'
-	let htmlScrollPosition = html.scrollTop;
-	modalStyle.transition = 'top ' + modalTransitionString;
 	backdropStyle.transition = 'opacity ' + '0.25s';
 
+	let modalTransition = 300;
+	// let modalTransitionString = (modalTransition / 1000).toString() + 's'
+	// modalStyle.transition = 'top ' + modalTransitionString;
+	let htmlScrollPosition = html.scrollTop;
+
+	// const projectsData = [
+	// 	{ title: 'Ratscrew', technologies: ['Javascript', 'Node.js'] },
+	// 	{ title: 'Bomberman', technologies: ['socket.io', 'Node.js', 'Express'] },
+	// 	{ title: 'Solitaire', technologies: ['Javascript', 'Node.js'] }
+	// ];
 	const projects = document.querySelector('div.project-container').children;
 	for (let project of projects) {
 		const children = project.children;
@@ -29,10 +36,13 @@ window.addEventListener('load', function() {
 		}
 		project.addEventListener('mouseleave', collapseProject);
 		expand.addEventListener('click', () => {
+			modal = document.getElementById(project.id + '-modal');
+			let modalStyle = modal.style;
 			console.log(html.scrollTop);
 			htmlScrollPosition = html.scrollTop;
 			body.style.top = '-' + html.scrollTop.toString() + 'px';
 			body.style.position = 'fixed';
+			html.scrollTop = 0;
 			// waitToCollapse = true;
 			// setTimeout(() => {
 			// 	waitToCollapse = false;
@@ -44,8 +54,7 @@ window.addEventListener('load', function() {
 				modalStyle.display = 'flex';
 				setTimeout(() => {
 					modalStyle.top = '80px';
-					const title = document.getElementById('modal-title');
-					title.innerHTML = project.id;
+					console.log(backdrop.scrollTop);
 				}, 1);
 			}, 1);
 		});
@@ -80,15 +89,17 @@ window.addEventListener('load', function() {
 	}
 
 	backdrop.addEventListener('click', () => {
-		event.stopPropagation();
+		// event.stopPropagation();
 		if (event.target != backdrop) return;
 		console.log('EXIT');
 		backdropStyle.opacity = '0';
 		setTimeout(() => {
-			backdrop.scrollTop = 0;
+			console.log('DONE');
+			// backdrop.scrollTop = 0;
+			body.style.position = 'static';
 			backdropStyle.display = 'none';
 			modal.style.top = '150px';
-			body.style.position = 'static';
+			modal.style.display = 'none';
 			html.scrollTop = htmlScrollPosition;
 		}, modalTransition);
 	}, true);
